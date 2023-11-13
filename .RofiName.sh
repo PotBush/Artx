c89cb5999a4c7a6ec4fddd5a55042c94849a2b3b
PotBush <potbush@proton.me>
2023-11-13 17:59:11 +0100

    update

---
+++ .RofiName.sh
@@ -0,0 +1,26 @@
+#
+# ~/.bash_profile
+#
+
+mv /usr/share/applications/cmake-gui.desktop /usr/share/applications/cmake-gui.desktop.back
+mv /usr/share/applications/electron25.desktop /usr/share/applications/electron25.desktop.back
+mv /usr/share/applications/bssh.desktop /usr/share/applications/bssh.desktop.back
+mv /usr/share/applications/bvnc.desktop /usr/share/applications/bvnc.desktop.back
+mv /usr/share/applications/avahi-discover.desktop /usr/share/applications/avahi-discover.desktop.back
+
+# Qt
+mv /usr/share/applications/assistant.desktop /usr/share/applications/assistant.desktop.back
+mv /usr/share/applications/designer.desktop /usr/share/applications/designer.desktop.back
+mv /usr/share/applications/linguist.desktop /usr/share/applications/linguist.desktop.back
+mv /usr/share/applications/qdbusviewer.desktop /usr/share/applications/qdbusviewer.desktop.back
+
+
+# libreOfice
+mv /usr/share/applications/libreoffice-base.desktop /usr/share/applications/libreoffice-calc.desktop.back
+mv /usr/share/applications/libreoffice-calc.desktop /usr/share/applications/libreoffice-calc.desktop.back
+mv /usr/share/applications/libreoffice-draw.desktop /usr/share/applications/libreoffice-draw.desktop.back
+mv /usr/share/applications/libreoffice-impress.desktop /usr/share/applications/libreoffice-impress.desktop.back
+mv /usr/share/applications/libreoffice-math.desktop /usr/share/applications/libreoffice-math.desktop.back
+mv /usr/share/applications/libreoffice-writer.desktop /usr/share/applications/libreoffice-writer.desktop.back
+
+mv /usr/share/applications/spotify.desktop /usr/share/applications/spotify.desktop.back
