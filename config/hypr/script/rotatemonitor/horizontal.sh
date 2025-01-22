b7ba2d8c28ba2b24f5626a6421410c44b455f800
PotBush <potbush@proton.me>
2025-01-22 18:55:10 +0100

    update

--- config/hypr/script/rotatemonitor/horizontal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/hypr/script/rotatemonitor/horizontalConf/monitorsHorizontal.conf ~/.config/hypr/monitors.conf
-cp ~/.config/hypr/script/rotatemonitor/horizontalConf/inputHorizontal.conf ~/.config/hypr/input.conf
-
-cp ~/.config/hypr/script/rotatemonitor/horizontalConf/waybarHorizontal.jsonc ~/.config/waybar/config.jsonc
-
-echo "horizontal" > ~/.config/hypr/script/rotatemonitor/status
--- config/hypr/script/startSystem.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sleep 1
-swww img ~/.config/hypr/Wallpaper/01.png
-
-XDG_MENU_PREFIX=arch- kbuildsycoca6
